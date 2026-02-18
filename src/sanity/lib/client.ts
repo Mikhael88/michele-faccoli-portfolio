b9d4b0dc692f9d022446aef59c9c5a7eb6fc9413
Mikhael88 <faccoli.michele@gmail.com>
2026-02-18 14:40:46 +0100

    Fix: added token to sanity client

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,10 +1,10 @@
 import { createClient } from 'next-sanity'
-
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: false, // CDN abilitato: perfetto per lettura pubblica (Portfolio, ecc.)
+  useCdn: false, // Disabilitato per avere dati sempre freschi (non cacheati dalla CDN)
+  token: process.env.SANITY_API_READ_TOKEN, // <--- AGGIUNGI QUESTA RIGA
 })
